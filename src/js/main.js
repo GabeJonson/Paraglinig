b2769e1185196775e8ff931cc3bd75aad026b289
Gabe Jonson <sky3107@gmail.com>
2017-06-24 12:11:30 +0300

    desctop paragliding container done'

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-
+  Paragliding.init();
 });
+
+var Paragliding = (function() {
+  return {
+    init: function() {
+      this.scroll();
+    },
+
+    scroll: function() {
+      var _this = this;
+
+      $(window).on('scroll', function() {
+        var sTop = $(this).scrollTop();
+
+        _this.parallax(sTop)
+      })
+    },
+
+    parallax: function(scrolling) {
+      var parallaxBg = $('.parallax');
+
+      parallaxBg.css('background-position', '50%' + -(scrolling / 2) + 'px');
+    }
+  }
+})();
